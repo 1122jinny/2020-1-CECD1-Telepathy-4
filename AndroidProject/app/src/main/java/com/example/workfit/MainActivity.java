71e5453fe561eb88b781daa0d9dafbb1ed66b023
nohyoonko <kny_1011@naver.com>
2020-09-08 14:53:30 +0900

    Add BottomNavigationView

---
+++ AndroidProject/app/src/main/java/com/example/workfit/MainActivity.java
@@ -0,0 +1,48 @@
+package com.example.workfit;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class MainActivity extends AppCompatActivity {
+    private FragmentManager fragmentManager = getSupportFragmentManager();
+    private FragmentHome fragmentHome = new FragmentHome();
+    private FragmentStatistic fragmentStatistic = new FragmentStatistic();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new ItemSelectedListener());
+    }
+    class ItemSelectedListener implements BottomNavigationView.OnNavigationItemSelectedListener{
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+            FragmentTransaction transaction = fragmentManager.beginTransaction();
+
+            switch(menuItem.getItemId())
+            {
+                case R.id.page_1:
+                    transaction.replace(R.id.frameLayout, fragmentHome).commitAllowingStateLoss();
+
+                    break;
+                case R.id.page_2:
+                    transaction.replace(R.id.frameLayout, fragmentStatistic).commitAllowingStateLoss();
+                    break;
+            }
+            return true;
+        }
+    }
+
+}
